2d6511d9ef0d65afe021dcb65e5841fd89ab0270
Vladimir Andreev <vladimir.andreev@youconsult.ru>
2021-05-08 06:25:32 +0300

    task1

--- task_1/solution.js
+++ task_1/solution.js
@@ -5,19 +5,19 @@ function addInCartMessage(title, price) {
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
-    let message;
+    let message; 
     // присвойте переменной message значение переменной productName
-    let message = productName;
+    let message = productName; 
     // добавьте в конец переменной message пробел
-    let message = message + " ";
+    let message = message + " "; 
     // добавьте в конец переменной message текст 'за'
-    let message = message + "за";
+    let message = message + "за"; 
     // добавьте в конец переменной message значение переменной productPrice
-    let message = message + productPrice;
+    let message = message + productPrice; 
     // добавьте в конец переменной message пробел
-    let message = message + " ";
+    let message = message + " "; 
     // добавьте в конец переменной message текст 'теперь в корзине!'
-    let message = message + "теперь в корзине!";
+    let message = message + "теперь в корзине!"; 
     // выведите переменную message в консоль
     console.log(message);
 
@@ -32,8 +32,11 @@ function addInCartCountChange(value) {
     let oldValue = value;
 
     // создайте переменную newValue
+    let newValue;
     // присвойте ей значение oldValue
+    let newValue = oldValue;
     // увеличьте значение newValue на 1
+    let newValue = newValue + 1;
 
     // Конец решения задания №1.2.
 
@@ -47,8 +50,11 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
+    let newSum = oldSum + difference;
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
+    let newSumText =  newSum + " ₽";
+    // замените склеивание, используемое в newSumText на шаблонную строку
+    let newSumText = `newSum  ₽`
 
     // Конец решения задания №1.3.
 
